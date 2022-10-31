6bffbc0d4d4058f1ee83f59205dfcf14362ab4dc
J-Prajwal <luckyjais058@gmail.com>
2022-10-31 19:57:54 +0530

    minor bug fix

--- cli.js
+++ cli.js
@@ -21,6 +21,7 @@ switch (template) {
     console.log(
       "Your template has been successfully added! \n\nFollow the steps below to get started: \n1. npm install \n2. npm start\n\n"
     );
+    break;
   }
   case "express-server": {
     try {
@@ -34,6 +35,7 @@ switch (template) {
     console.log(
       "Your template has been successfully added! \n\nFollow the steps below to get started: \n1. npm install \n2. npm start\n\n"
     );
+    break;
   }
   case "mern-chakra-vite": {
     try {
@@ -51,6 +53,7 @@ switch (template) {
     console.log(
       "Your template has been successfully added! \n\nFollow the steps below to get started: \n1. cd client \n2. npm install \n3. npm run start \n4. cd .. \n5. cd server \n6. npm install\n7. npm run start\n\n"
     );
+    break;
   }
   default: {
     console.log("Happy Coding!");
